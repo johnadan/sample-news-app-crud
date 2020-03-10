5c3a0894cf555ce384d4dd4b1b751aa3d503c328
John McLem Adan <jadan@i4asiacorp.com>
2020-03-10 16:52:31 +0800

    sample news crud app

---
+++ www/resources/views/editnew.blade.php
@@ -0,0 +1,56 @@
+@extends('layouts.app')
+
+@section('content')
+
+{{-- <style>
+  .uper {
+    margin-top: 40px;
+  }
+</style> --}}
+
+<div class="card uper">
+  <div class="card-header">
+    Edit News
+  </div>
+  <div class="card-body">
+    @if ($errors->any())
+      <div class="alert alert-danger">
+        <ul>
+            @foreach ($errors->all() as $error)
+              <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+      </div><br />
+    @endif
+
+      <form id="editNew" method="POST" action="{{ route('news.update', $new->id) }}">
+      	{{ csrf_field() }}
+        {{ method_field('PUT')}}
+
+        <div class="form-group">
+          <label class="col-form-label" for="new-title">Title:</label>
+          <input type="text" class="form-control" name="editedtitle" value="{{ $new->title }}"></input>
+        </div>
+        <div class="form-group">
+          <label class="col-form-label" for="new-author">Author:</label>
+          <input type="text" class="form-control" name="editedauthor" value="{{ $new->author }}"></input>
+        </div>
+        <div class="form-group">
+          <label class="col-form-label" for="new-date">Date:</label>
+          <input type="text" class="form-control" name="editeddate" value="{{ $new->date }}"></input>
+        </div>
+        <div class="form-group">
+          <label class="col-form-label" for="new-content">Content:</label>
+          <textarea class="form-control" rows="10" name="editedcontent">{{ $new->content }}</textarea>
+        </div>
+        {{-- <div class="form-group">
+          <label for="news-image">Featured Image:</label>
+          <input type="file" class="form-control-file" name="editedimage">
+        </div> --}}
+        <button type="submit" id="editModalBtn" class="btn btn-success" data-id="{{$new->id}}">Save</button>
+      </form>
+      <p class="mt-5"><a href="{{ route('home') }}">Back</p>  
+  </div>
+</div>
+
+@endsection
---
+++ www/resources/views/home.blade.php
@@ -0,0 +1,84 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+
+   @if(session()->get('success'))
+    <div class="alert alert-success">
+      {{ session()->get('success') }}  
+    </div><br />
+    @endif
+
+  <div class="row justify-content-center">
+    <div class="col-md-12">
+        <div class="card">
+            <div class="card-header">CMS News</div>
+
+            <div class="card-body">
+                @if (session('status'))
+                    <div class="alert alert-success" role="alert">
+                        {{ session('status') }}
+                    </div>
+                @endif
+
+                @if($errors->any())
+                  <div class="alert alert-danger">
+                      @foreach($errors->all() as $error)
+                          <p>{{ $error }}</p>
+                      @endforeach
+                  </div>
+                @endif
+
+                <button class="btn btn-warning mb-2" onclick="window.location='{{ url("/createnew") }}'"><img src=https://cdn.jsdelivr.net/npm/bootstrap-icons/icons/plus.svg width="32" height="32" alt="">Add New</button>
+
+                <button class="btn btn-secondary mb-2" onclick="window.location='{{ url("/search") }}'"><img src=https://cdn.jsdelivr.net/npm/bootstrap-icons/icons/search.svg width="32" height="32" alt="">Search</button>
+
+            </div>
+
+            <table class="table">
+              <thead>
+                <tr>
+                  <th scope="col">Title</th>
+                  <th scope="col">Author</th>
+                  <th scope="col">Date</th>
+                  <th scope="col">Created At</th>
+                  <th scope="col">Content</th>
+                  <th scope="col">Image</th>
+                  <th scope="col">Actions</th>
+                </tr>
+              </thead>
+              <tbody>
+
+                @foreach($news as $new)
+
+                <tr>
+                  <th scope="row">{{ $new->title }}</th>
+                  <td>{{ $new->author }}</td>
+                  <td>{{ $new->date }}</td>
+                  <td>{{ $new->created_at }}</td>
+                  <td>{{ $new->content }}</td>
+                  <td>{{ $new->image }}</td>
+                  <td>
+
+                    <button type="button" class="btn btn-primary mb-2" data-toggle="modal" data-target="#editModal" onclick="window.location='{{ route('news.edit', $new->id) }}'"><i class="fa fa-edit"></i></button>
+                    <form action="{{ route('news.destroy', $new->id)}}" method="post">
+                      {{ csrf_field() }}
+                      {{ method_field('DELETE') }}
+                      
+                      <button class="btn btn-danger" type="submit"><i class="fa fa-trash-alt"></i></button>
+                    </form>
+                    
+                  </td>
+                </tr>
+
+                @endforeach
+
+              </tbody>
+            </table>
+        </div>
+      </div>
+  </div>
+
+</div>
+
+@endsection
